d0b2451cc6362acdf8ffd08e6ae0b2e7f9d51f5b
Piumir3 <piumi.18@itfac.mrt.ac.lk>
2021-11-27 19:12:26 +0530

    added log record

---
+++ src/main/react-app/src/components/view/charts/chart.component.js
@@ -0,0 +1,278 @@
+import React, {Component} from "react";
+import axios from "axios";
+import {Bar, Line} from 'react-chartjs-2'
+import {Card} from "react-bootstrap";
+import {round} from "@tensorflow/tfjs-core";
+
+class Chart extends Component {
+    state = {
+        loading: false,
+        data1: [],
+        labels1: [[]],
+        data2: [],
+        labels2: [[]],
+        data3: [],
+        labels3: [[]],
+        data4: [],
+        labels4: [[]],
+        data5: [],
+        labels5: [[]],
+    };
+
+    async componentDidMount() {
+        await axios
+            .get(
+                `http://localhost:8080/api/auth/log?fileId=${this.props.match.params.id}`
+            )
+            .then((res) => {
+                //1st chart
+                const dataArrayY1 = [];
+                res.data.map(item => {
+                    dataArrayY1.push(item.source)
+                })
+                let tmp1 = [];
+                let count1 = 1
+                res.data.map((o) => {
+                    const existing = tmp1.find(e => e.source === o.source);
+                    if (existing) {
+                        existing.count1++;
+                    } else {
+                        tmp1.push({count1: count1, source: o.source});
+                    }
+                })
+                this.setState({
+                    data1: tmp1.map(o => o.count1),
+                    labels1: tmp1.map(o => o.source),
+                });
+                //2nd chart
+                const dataArrayY2 = [];
+                res.data.map(item => {
+                    dataArrayY2.push(item.length)
+                })
+                const dataArrayX2 = []
+                res.data.map(item => {
+                    dataArrayX2.push(item.source)
+                })
+                this.setState({
+                    data2: dataArrayY2,
+                    labels2: dataArrayX2,
+                });
+                //3rd chart
+                const dataArrayY3 = [];
+                res.data.map(item => {
+                    dataArrayY3.push(item.length)
+                })
+                const dataArrayX3 = []
+                res.data.map(item => {
+                    dataArrayX3.push(item.destination)
+                })
+                this.setState({
+                    data3: dataArrayY3,
+                    labels3: dataArrayX3,
+                });
+                //4th chart
+                const dataArrayY4 = [];
+                res.data.map(item => {
+                    dataArrayY4.push(item.destination)
+                })
+                let tmp4 = [];
+                let count4 = 1;
+                res.data.map((o) => {
+                    const existing = tmp4.find(e => e.destination === o.destination);
+                    if (existing) {
+                        existing.count4++;
+                    } else {
+                        tmp4.push({count4: count4, destination: o.destination});
+                    }
+                })
+                this.setState({
+                    data4: tmp4.map(o => o.count4),
+                    labels4: tmp4.map(o => o.destination),
+                });
+                //5th chart
+                const dataArrayY5 = [];
+                res.data.map(item => {
+                    dataArrayY5.push(item.protocol)
+                })
+                let tmp5 = [];
+                let count5 = 1;
+                res.data.map((o) => {
+                    const existing = tmp5.find(e => e.protocol === o.protocol);
+                    if (existing) {
+                        existing.count5++;
+                    } else {
+                        tmp5.push({count5: count5, protocol: o.protocol});
+                    }
+                })
+                this.setState({
+                    data5: tmp5.map(o => o.count5),
+                    labels5: tmp5.map(o => o.protocol),
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+        this.setState({
+            loading: true,
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Card border="primary" style={{width: '95rem', height: '25rem'}}>
+                    <Card.Body>
+                        <Card.Title>No. of times each source address accessed</Card.Title>
+                        <Bar
+                            data={{
+                                labels: this.state.labels1,
+                                datasets: [{
+                                    label: 'X-axis: Source address, Y-axis: Count',
+                                    data: this.state.data1,
+                                    backgroundColor: '#FFFF66',
+                                    scaleBeginAtZero: true,
+                                    borderColor: 'red',
+                                    borderWidth: 2
+                                }],
+                            }}
+                            height={20}
+                            width={95}
+                            options={{
+                                maintainAspectRatio: false, scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true,
+                                            min: 0
+                                        }
+                                    }]
+                                }
+                            }}
+                        />
+                    </Card.Body>
+                </Card>
+                <Card border="primary" style={{width: '95rem', height: '25rem'}}>
+                    <Card.Body>
+                        <Card.Title>Packet Length of each source addresses</Card.Title>
+                        <Line
+                            data={{
+                                labels: this.state.labels2,
+                                datasets: [{
+                                    label: 'X-axis: Source address, Y-axis: Packet Length',
+                                    data: this.state.data2,
+                                    borderColor: 'blue',
+                                    backgroundColor: '#99ccff',
+                                    scaleBeginAtZero: true,
+                                    lineJoin: round
+                                }],
+                            }}
+                            height={20}
+                            width={95}
+                            options={{
+                                maintainAspectRatio: false, scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true,
+                                            min: 0
+                                        }
+                                    }]
+                                }
+                            }}
+                        />
+                    </Card.Body>
+                </Card>
+                <Card border="primary" style={{width: '95rem', height: '25rem'}}>
+                    <Card.Body>
+                        <Card.Title>Packet Length of each destination addresses</Card.Title>
+                        <Line
+                            data={{
+                                labels: this.state.labels3,
+                                datasets: [{
+                                    label: 'X-axis: Destination address, Y-axis: Packet Length',
+                                    data: this.state.data3,
+                                    borderColor: 'green',
+                                    backgroundColor: '#90ee90',
+                                    scaleBeginAtZero: true,
+                                }],
+                            }}
+                            height={20}
+                            width={95}
+                            options={{
+                                maintainAspectRatio: false, scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true,
+                                            min: 0
+                                        }
+                                    }]
+                                }
+                            }}
+                        />
+                    </Card.Body>
+                </Card>
+                <Card border="primary" style={{width: '95rem', height: '25rem'}}>
+                    <Card.Body>
+                        <Card.Title>No. of times each destination address accessed</Card.Title>
+                        <Bar
+                            data={{
+                                labels: this.state.labels4,
+                                datasets: [{
+                                    label: 'X-axis: Destination address, Y-axis: Count',
+                                    data: this.state.data4,
+                                    backgroundColor: '#fed8b1',
+                                    scaleBeginAtZero: true,
+                                    borderWidth: 2,
+                                    borderColor: 'Maroon'
+                                }],
+                            }}
+                            height={20}
+                            width={95}
+                            options={{
+                                maintainAspectRatio: false, scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true,
+                                            min: 0
+                                        }
+                                    }]
+                                }
+                            }}
+                        />
+                    </Card.Body>
+                </Card>
+                <Card border="primary" style={{width: '95rem', height: '25rem'}}>
+                    <Card.Body>
+                        <Card.Title>No. of times each Protocol accessed</Card.Title>
+                        <Bar
+                            data={{
+                                labels: this.state.labels5,
+                                datasets: [{
+                                    label: 'X-axis: Type of Protocol, Y-axis: Count',
+                                    data: this.state.data5,
+                                    backgroundColor: '#b19cd9',
+                                    scaleBeginAtZero: true,
+                                    borderWidth: 2,
+                                    borderColor: 'Purple'
+                                }],
+                            }}
+                            height={20}
+                            width={95}
+                            options={{
+                                maintainAspectRatio: false, scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true,
+                                            min: 0
+                                        }
+                                    }]
+                                }
+                            }}
+                        />
+                    </Card.Body>
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default Chart;
---
+++ src/main/react-app/src/components/view/home/profile.component.js
@@ -0,0 +1,195 @@
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import AuthService from "../../../services/auth.service";
+import "../../pages/css/profile.css";
+import { Figure, Button } from "react-bootstrap";
+import userPic from "../../pages/images/user.png";
+import axios from "axios";
+import HashLoader from "react-spinners/HashLoader";
+
+export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      redirect: null,
+      theme: "#d3d5fd",
+      userReady: false,
+      loading: false,
+      name: "",
+      phone: "",
+      role: "",
+      currentUser: { username: "" },
+    };
+  }
+
+  componentDidMount() {
+    document.title = "Profile";
+    const currentUser = AuthService.getCurrentUser();
+    const userId = currentUser.id;
+    if (userId) {
+      this.findUserById(userId);
+    } else if (!currentUser) this.setState({ redirect: "./" });
+    this.setState({
+      currentUser: currentUser,
+      userReady: true,
+      name: this.state.name,
+    });
+  }
+
+  findUserById = (userId) => {
+    axios
+      .get("http://localhost:8080/api/auth/" + userId)
+      .then((response) => {
+        if (response.data != null) {
+          this.setState({
+            id: response.data.id,
+            name: response.data.name,
+            username: response.data.username,
+            nic: response.data.nic,
+            phone: response.data.phone,
+            theme: response.data.theme,
+          });
+          let themec = this.state.theme;
+          switch (themec) {
+            case "#d3d5fd":
+              themec = "outline-dark";
+              break;
+            case "#0062cc":
+              themec = "primary";
+              break;
+            case "#239d60":
+              themec = "success";
+              break;
+            case "#3e3e3e":
+              themec = "dark";
+              break;
+            default:
+              break;
+          }
+          this.setState({ theme: themec, loading: true });
+        }
+      })
+      .catch((error) => {
+        console.error("Error - " + error);
+        this.setState({ redirect: "/home" });
+      });
+  };
+
+  render() {
+    if (this.state.redirect) {
+      return <Redirect to={this.state.redirect} />;
+    }
+
+    const { currentUser, theme } = this.state;
+
+    return (
+      <div>
+        {this.state.loading ? (
+          <div className="container">
+            {this.state.userReady ? (
+              <div>
+                <div className="page-content container" id="page-content">
+                  <div className="">
+                    <div className="row container">
+                      <div className="col-xl-12 col-md-12">
+                        <div className="row m-l-0 m-r-0">
+                          <div className="col-sm-4 bg-c-lite-color user-profile">
+                            <div
+                              style={{ color: "black" }}
+                              className="card-block text-center "
+                            >
+                              <div className="m-b-25 ">
+                                <Figure>
+                                  <Figure.Image
+                                    width={120}
+                                    height={120}
+                                    alt="120x120"
+                                    src={userPic}
+                                  />
+                                  <Figure.Caption>
+                                    Profile Picture
+                                  </Figure.Caption>
+                                </Figure>
+                              </div>
+                              <h4 className="f-w-600 name-style">
+                                {this.state.name}
+                              </h4>
+                              <br />
+
+                              {currentUser.roles.includes("ROLE_ADMIN") ? (
+                                <p>Admin</p>
+                              ) : (
+                                <p>
+                                  User <br />
+                                </p>
+                              )}
+                            </div>
+                          </div>
+                          <div className="col-sm-8">
+                            <div className="card-block-right">
+                              <h6 className="m-b-20 p-b-5 b-b-default f-w-600">
+                                Basic Information
+                              </h6>
+                              <div className="row">
+                                <div className="col-sm-6">
+                                  <p className="m-b-10 f-w-600">Name</p>
+                                  <h6 className="text-muted f-w-400">
+                                    {this.state.name}
+                                  </h6>
+                                  <br />
+                                  <br />
+                                </div>
+                                <div className="col-sm-6">
+                                  <p className="m-b-10 f-w-600">NIC Number</p>
+                                  <h6 className="text-muted f-w-400">
+                                    {currentUser.nic}
+                                  </h6>
+                                </div>
+                              </div>
+                              <h6 className="m-b-20 p-b-5 b-b-default f-w-600">
+                                Contact Information
+                              </h6>
+                              <div className="row">
+                                <div className="col-sm-6">
+                                  <p className="m-b-10 f-w-600">Email</p>
+                                  <h6 className="text-muted f-w-400">
+                                    {this.state.username}
+                                  </h6>
+                                </div>
+                                <div className="col-sm-6 m-b-10">
+                                  <p className="m-b-10 f-w-600">Telephone</p>
+                                  <h6 className="text-muted f-w-400">
+                                    {this.state.phone}
+                                  </h6>
+                                  <br />
+                                  <br />
+                                </div>
+                              </div>
+                              <div className="row m-t-button m-l-5">
+                                <Button
+                                  href={"edit/userId=?" + currentUser.id}
+                                  variant={theme}
+                                >
+                                  Edit Profile
+                                </Button>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : null}
+          </div>
+        ) : (
+          <div className="text-center" style={{ marginTop: "20%" }}>
+            <HashLoader color={"#292b2c"} loading={true} size={150} />
+          </div>
+        )}
+      </div>
+    );
+  }
+}

2e202ef5e0ac41333788f32132e4bd6ed6ec2ea4
gimsara <gimsarakulathunga2@gmail.com>
2021-11-27 18:44:46 +0530

    added customer forms and user functions

---
+++ src/main/react-app/src/components/customerRequests/customerForm.js
@@ -0,0 +1,291 @@
+import React, { Component } from "react";
+import "../../pages/css/customerFormStyle.css";
+
+import { withStyles } from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
+
+import { Row, Jumbotron, Form, Button } from "react-bootstrap";
+
+import IconButton from "@material-ui/core/IconButton";
+import ListAltIcon from "@material-ui/icons/ListAlt";
+import Tooltip from "@material-ui/core/Tooltip";
+import axios from "axios";
+
+
+const validEmailRegex = RegExp(
+  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+);
+const validNic = RegExp(/^([0-9]{9}[x|X|v|V]|[0-9]{12})$/);
+
+
+class CustomerForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = this.intialState;
+    this.custChange = this.custChange.bind(this);
+    this.submitCustomer = this.submitCustomer.bind(this);
+  }
+
+  intialState = {
+    cname: "",
+    successful: false,
+    message: "",
+    cemail: "",
+    cphone: "",
+    creason: "",
+    cnic: "",
+    alert: "",
+    errors: {
+      cname: "",
+      cemail: "",
+      cphone: "",
+      cnic: "",
+      creason: "",
+    },
+  };
+
+  componentDidMount() {
+    document.title = "Form";
+  }
+
+  submitCustomer = (event) => {
+    event.preventDefault();
+
+    let cemail = this.state.cemail.toLowerCase();
+    let date = new Date().toLocaleString() + "";
+
+    const customerReq = {
+      c_name: this.state.cname,
+      email: cemail,
+      phone: this.state.cphone,
+      reason: this.state.creason,
+      c_nic: this.state.cnic,
+      date: date,
+    };
+
+    axios
+      .post("http://localhost:8080/api/auth/customer", customerReq)
+      .then((response) => {
+        if (response.data != null) {
+          this.setState({ message: "success", successful: true });
+          setTimeout(() => this.setState({ message: "" }), 3000);
+          setTimeout(() => this.setState(this.intialState), 2000);
+          console.log("Succesfully added");
+        }
+        //this.setState(this.intialState);
+      })
+      .catch((error) => {
+        this.setState({ message: "error" });
+        setTimeout(() => this.setState({ message: "" }), 3000);
+      });
+  };
+
+  custChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+    let errors = this.state.errors;
+
+    switch (name) {
+      case "cname":
+        errors.cname =
+          value.length < 5 ? "Name must be at least 5 characters long!" : "";
+        break;
+      case "cemail":
+        errors.cemail = validEmailRegex.test(value)
+          ? ""
+          : "Email is not valid!";
+        break;
+      case "cnic":
+        errors.cnic = validNic.test(value) ? "" : "NIC is not valid!";
+        break;
+      case "cphone":
+        errors.cphone =
+          value.length < 10 ? "Phone Number must be 10 characters long!" : "";
+        break;
+      default:
+        break;
+    }
+    this.setState({ errors, [name]: value });
+  };
+
+  render() {
+    const { cname, cemail, cnic, cphone, creason, errors } = this.state;
+
+    return (
+      <div>
+        <div className="container mgntop">
+          <h4 style={{ textAlign: "center" }}> Request Details </h4>
+          <Row>
+            <div className="wrapper p-b-45">
+              <div style={{ textAlign: "right" }}>
+                <Tooltip title="Request Details" placement="top">
+                  <IconButton
+                    href="/home/request"
+                    aria-label="delete"
+                    ontSize="small"
+                  >
+                  
+                    <ListAltIcon fontSize="small" />
+                  </IconButton>
+                </Tooltip>
+              </div>
+
+              <Form onSubmit={this.submitCustomer}>
+                <Jumbotron style={{ width: "60rem" }}>
+                  <div>
+                    <div>
+                      <div className="card card-5">
+                        <div className="card-heading">
+                          <h4 className="title">
+                            Customer Requst Details Collecting Form
+                          </h4>
+                          <br />
+                        </div>
+                        <div className="card-body">
+                          <form id="customerForm">
+                            <div className="form-row">
+                              <div className="name"> Customer Name </div>
+                              <div className="value">
+                                <div>
+                                  {errors.cname.length > 0 && (
+                                    <span className="error-form">
+                                      {errors.cname}
+                                    </span>
+                                  )}
+                                </div>
+                                <div className="input-group">
+                                  <input
+                                    className="input--style-5"
+                                    placeholder="Name here"
+                                    type="text"
+                                    id="name"
+                                    name="cname"
+                                    value={cname}
+                                    autoComplete="off"
+                                    onChange={this.custChange}
+                                    required
+                                  />
+                                </div>
+                              </div>
+                            </div>
+                            <div className="form-row">
+                              <div className="name"> NIC No </div>
+                              <div className="value">
+                                {errors.cnic.length > 0 && (
+                                  <span className="error-form">
+                                    {errors.cnic}
+                                  </span>
+                                )}
+                                <div className="input-group">
+                                  <input
+                                    className="input--style-5"
+                                    type="text"
+                                    value={cnic}
+                                    name="cnic"
+                                    placeholder="NIC Number "
+                                    autoComplete="off"
+                                    onChange={this.custChange}
+                                    required
+                                  />
+                                </div>
+                              </div>
+                            </div>
+                            <div className="form-row">
+                              <div className="name"> Email </div>
+                              <div className="value">
+                                {errors.cemail.length > 0 && (
+                                  <span className="error-form">
+                                    {errors.cemail}
+                                  </span>
+                                )}
+                                <div className="input-group">
+                                  <input
+                                    className="input--style-5"
+                                    type="email"
+                                    value={cemail}
+                                    name="cemail"
+                                    placeholder="Email here"
+                                    autoComplete="off"
+                                    onChange={this.custChange}
+                                    required
+                                  />
+                                </div>
+                              </div>
+                            </div>
+                            <div className="form-row">
+                              <div className="name"> Contact Number </div>
+                              <div className="value">
+                                {errors.cphone.length > 0 && (
+                                  <span className="error-form">
+                                    {errors.cphone}
+                                  </span>
+                                )}
+                                <div className="input-group">
+                                  <input
+                                    className="input--style-5"
+                                    type="text"
+                                    value={cphone}
+                                    name="cphone"
+                                    placeholder="Contact No here"
+                                    onChange={this.custChange}
+                                    autoComplete="off"
+                                    required
+                                  />
+                                </div>
+                              </div>
+                            </div>
+                            <div className="form-row">
+                              <div className="name"> Reason / Request </div>
+                              <div className="value">
+                                <div className="input-group">
+                                  <input
+                                    className="input--style-5"
+                                    type="text"
+                                    value={creason}
+                                    placeholder="Reason for request"
+                                    name="creason"
+                                    autoComplete="off"
+                                    onChange={this.custChange}
+                                    required
+                                  />
+                                </div>
+                              </div>
+                            </div>
+                            <div>
+                              <Button type="submit" variant="warning">
+                                {" "}
+                                Submit{" "}
+                              </Button>
+                            </div>
+                            <br />
+                            <br />
+                            <div style={{ alignItems: "center" }}>
+                              <br /> &nbsp;&nbsp;{" "}
+                            </div>
+                            {this.state.message === "error" ? (
+                              <div className="alert alert-danger" role="alert">
+                                Please provide valid information!
+                              </div>
+                            ) : null}
+                            {this.state.message === "success" ? (
+                              <div className="alert alert-success" role="alert">
+                                Successfully added a new customer request
+                              </div>
+                            ) : null}
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </Jumbotron>
+              </Form>
+            </div>
+          </Row>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CustomerForm;
---
+++ src/main/react-app/src/components/view/user/userDetails.js
@@ -0,0 +1,237 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import AuthService from "../../../services/auth.service";
+
+import { Table, Button } from "react-bootstrap";
+
+import axios from "axios";
+import IconButton from "@material-ui/core/IconButton";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+import Tooltip from "@material-ui/core/Tooltip";
+import HashLoader from "react-spinners/HashLoader";
+
+class UserDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      role: undefined,
+      userdetails: [],
+      loading: false,
+      isAdminShow: true,
+    };
+  }
+
+  componentDidMount() {
+    document.title = "User Details";
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      this.setState({
+        currentUser: user,
+        showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+      });
+    }
+    axios
+      .get("http://localhost:8080/api/auth/users")
+      .then((response) => response.data)
+      .then((data) => {
+        this.setState({ userdetails: data, loading: true });
+      });
+    this.setState({
+      loading: false,
+    });
+  }
+
+  userShow = () => {
+    const { isAdminShow } = this.state;
+    this.setState({ isAdminShow: !isAdminShow });
+  };
+  deleteUser = (userId) => {
+    axios
+      .delete("http://localhost:8080/api/auth/" + userId)
+      .then((response) => {
+        if (response.data != null) {
+          this.setState({
+            userdetails: this.state.userdetails.filter(
+              (data) => data.id !== userId
+            ),
+          });
+        } else {
+          this.setState({ show: false });
+        }
+
+      });
+  };
+
+  render() {
+    const { isAdminShow, currentUser } = this.state;
+    return (
+      <div>
+        <div className="container mgntop">
+          {!this.state.loading ? (
+            <div className="text-center" style={{ marginTop: "20%" }}>
+              <HashLoader color={"#292b2c"} loading={true} size={150} />
+            </div>
+          ) : (
+            <div>
+              <h4 style={{ textAlign: "center" }}>
+                {" "}
+                User Details <br />
+              </h4>
+              <br />
+              {isAdminShow ? (
+                <Button
+                  style={{ float: "right" }}
+                  href="#"
+                  variant="light"
+                  size="sm"
+                  onClick={this.userShow}
+                >
+                  Show Regular Users
+                </Button>
+              ) : (
+                <Button
+                  size="sm"
+                  href="#"
+                  variant="light"
+                  style={{ float: "right" }}
+                  onClick={this.userShow}
+                >
+                  Show Admins
+                </Button>
+              )}
+              <br />
+              {isAdminShow ? (
+                <div>
+                  <h6>Admins</h6>
+                  <br />
+                  <Table responsive striped bordered hover size="sm">
+                    <thead>
+                      <tr style={{ color: "white", background: "#343A40" }}>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Telephone No.</th>
+                        <th>Joined</th>
+                      </tr>
+                    </thead>
+
+                    {this.state.userdetails.length === 0 ? (
+                      <tr>
+                        <td>Wait ...</td>
+                      </tr>
+                    ) : (
+                      this.state.userdetails.map((user, i) => (
+                        <tbody>
+                          {user.roles &&
+
+                            user.id !== currentUser.id &&
+                            user.roles.map((role, index) => (
+                              <tr key={index}>
+                                {role.name === "ROLE_ADMIN" ? (
+                                  <td>{user.name}</td>
+                                ) : null}
+                                {role.name === "ROLE_ADMIN" ? (
+                                  <td>{user.username}</td>
+                                ) : null}
+                                {role.name === "ROLE_ADMIN" ? (
+                                  <td>{user.phone}</td>
+                                ) : null}
+                                {role.name === "ROLE_ADMIN" ? (
+                                  <td>{user.date}</td>
+                                ) : null}
+                              </tr>
+
+                            ))}
+                          {user.id === currentUser.id && (
+                            <tr
+                              style={{ color: "black", background: "#A9C2E3" }}
+                            >
+                              <td>{user.name}</td>
+                              <td>{user.username}</td>
+                              <td>{user.phone}</td>
+                              <td>{user.date}</td>
+                            </tr>
+                          )}
+                        </tbody>
+                      ))
+                    )}
+                  </Table>
+                </div>
+              ) : (
+                <div>
+                  <h6>Users</h6>
+                  <br />
+                  <Table responsive striped bordered hover size="sm">
+                    <thead>
+                      <tr style={{ color: "white", background: "#343A40" }}>
+                        <th className="ml-3">#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Telephone No.</th>
+                        <th>Joined</th>
+                        <th style={{ textAlign: "center" }}>
+                          <span>Modify</span>
+                        </th>
+                      </tr>
+                    </thead>
+
+                    {this.state.userdetails.length === 0 ? (
+                      <tr>
+                        <td>No</td>
+                      </tr>
+                    ) : (
+                      this.state.userdetails.map((user, i) => (
+                        <tbody>
+                          {user.roles &&
+                            user.roles.map((role, index) => (
+                              <tr key={index}>
+                                {role.name === "ROLE_USER" ? (
+                                  <td>{++i}</td>
+                                ) : null}
+                                {role.name === "ROLE_USER" ? (
+                                  <td>{user.name}</td>
+                                ) : null}
+                                {role.name === "ROLE_USER" ? (
+                                  <td>{user.username}</td>
+                                ) : null}
+                                {role.name === "ROLE_USER" ? (
+                                  <td>{user.phone}</td>
+                                ) : null}
+                                {role.name === "ROLE_USER" ? (
+                                  <td>{user.date}</td>
+                                ) : null}
+                                {role.name === "ROLE_USER" ? (
+                                  <td>
+                                    <Tooltip
+                                      title={"delete " + user.username}
+                                      placement="right"
+                                    >
+                                      <IconButton
+                                        aria-label="delete"
+                                        onClick={this.deleteUser.bind(
+                                          this,
+                                          user.id
+                                        )}
+                                        fontSize="small"
+                                      >
+                                        <HighlightOffIcon fontSize="small" />
+                                      </IconButton>
+                                    </Tooltip>
+                                  </td>
+                                ) : null}
+                              </tr>
+                            ))}
+                        </tbody>
+                      ))
+                    )}
+                  </Table>
+                </div>
+              )}
+            </div>
+          )}
+        </div><br/>
+      </div>
+    );
+  }
+}
+export default withRouter(UserDetails);
